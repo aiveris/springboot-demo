c01e7f071afaa3df34705714c2b8cc0061855c31
aiveris <staind0323>
2019-11-02 15:21:33 +0200

    2019-11-02

---
+++ src/test/java/sda/springboot/exercises/springbootdemo/SpringbootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package sda.springboot.exercises.springbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
